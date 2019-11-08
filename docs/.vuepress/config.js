beea5012f3e3227037e537ae1ba8bb3aa64a9604
ifuture-pro <strongrobot@163.com>
2019-11-08 23:00:17 +0800

    add feed

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,14 +54,15 @@ module.exports = {
       }
     },
     "valineConfig": {
-      appId: 'ye1OoWUwxvVJ0CYfkOeSev7E-gzGzoHsz',
-      appKey: 'PNcuKvg9I0fUCdLVWTITSmBQ',
+      appId: '$valine_appId',
+      appKey: '$valine_appKey',
     },
     "logo": "/head.png",
     "GAID": "UA-151103127-1",
     "search": true,
     "searchMaxSuggestions": 10,
     "sidebar": "auto",
+    "sidebarDepth": 2,
     "lastUpdated": "Last Updated",
     "author": "ifuture",
     "record": "For a better yourself",
@@ -71,6 +72,21 @@ module.exports = {
     "lineNumbers": true,
   },
   "plugins": [
-    'flowchart'
+    'flowchart',
+    [require('../../packages/vuepress-plugin-rss'), {
+      type:['atom','rss2','podcast'],
+      limit: 20,
+      feedOptions : {
+        title: '未来编写者',
+        description: '未来编写者,做好自己，做更好的自己',
+        site_url: 'https://note.ifuture.pro',
+        image_url: 'https://note.ifuture.pro/head.png',
+        author: '未来编写者',
+        managingEditor: '未来编写者',
+        webMaster: '未来编写者',
+        copyright: '未来编写者',
+        language: 'zh'
+      }
+    }]
   ]
 }
