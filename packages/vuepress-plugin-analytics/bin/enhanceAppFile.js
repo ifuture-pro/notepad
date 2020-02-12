b0a46408ca73b4f5de1c83128a6b36a31e8543de
ifuture-pro <strongrobot@163.com>
2020-02-12 17:30:43 +0800

    change  to hexo

--- packages/vuepress-plugin-analytics/bin/enhanceAppFile.js
+++
@@ -1,5 +0,0 @@
-import Analytics from './Analytics'
-
-export default ({ Vue }) => {
-  Vue.component('GA', Analytics)
-}
--- packages/vuepress-plugin-analytics/index.js
+++
@@ -1,6 +0,0 @@
-const { resolve }  = require('path')
-
-module.exports = {
-  name:'@ifuture/vuepress-plugin-analytics',
-  enhanceAppFiles: resolve(__dirname, 'bin/enhanceAppFiles.js')
-}
